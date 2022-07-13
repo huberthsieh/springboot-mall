8429d1c12c2f0eb22e6af514a0bbcbb279372c2a
Hubert Hsieh <huberthsieh@HubertHsieh-2.local>
2022-07-13 11:51:51 +0800

    fix/修改 商品功能 - 實作查詢商品列表 分頁方式添加回傳總筆數

--- src/main/java/com/huberthsieh/springbootmall/dao/ProductDao.java
+++ src/main/java/com/huberthsieh/springbootmall/dao/ProductDao.java
@@ -7,6 +7,9 @@ import com.huberthsieh.springbootmall.model.Product;
 import java.util.List;
 
 public interface ProductDao {
+
+    Integer countProduct(ProductQueryParams productQueryParams);
+
     List<Product> getProducts(ProductQueryParams productQueryParams);
 
     Product getProductById(Integer productId);
