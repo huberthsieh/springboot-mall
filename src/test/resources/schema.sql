f4b473f8c1090e64fe7ceb56584d5507d7475d47
Hubert Hsieh <huberthsieh@HubertHsieh-2.local>
2022-07-29 16:02:29 +0800

    feat/新增 訂單功能 - 單元測試

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,3 +1,4 @@
+-- product
 CREATE TABLE IF NOT EXISTS product
 (
     product_id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -11,6 +12,7 @@ CREATE TABLE IF NOT EXISTS product
     last_modified_date TIMESTAMP    NOT NULL
     );
 
+-- user
 CREATE TABLE IF NOT EXISTS user
 (
     user_id            INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -19,3 +21,23 @@ CREATE TABLE IF NOT EXISTS user
     created_date       TIMESTAMP    NOT NULL,
     last_modified_date TIMESTAMP    NOT NULL
     );
+
+-- order
+CREATE TABLE IF NOT EXISTS `order`
+(
+    order_id           INT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id            INT       NOT NULL,
+    total_amount       INT       NOT NULL,
+    created_date       TIMESTAMP NOT NULL,
+    last_modified_date TIMESTAMP NOT NULL
+);
+
+-- order_item
+CREATE TABLE IF NOT EXISTS order_item
+(
+    order_item_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id      INT NOT NULL,
+    product_id    INT NOT NULL,
+    quantity      INT NOT NULL,
+    amount        INT NOT NULL
+);
